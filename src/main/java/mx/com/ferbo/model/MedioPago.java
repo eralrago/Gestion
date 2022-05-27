369932e8abeb3867d1c17268d203522ea451ffc7
Angel Lopez <67379810+anlosa@users.noreply.github.com>
2022-05-27 17:53:33 -0500

    Update MedioPago.java

--- src/main/java/mx/com/ferbo/model/MedioPago.java
+++ src/main/java/mx/com/ferbo/model/MedioPago.java
@@ -9,6 +9,8 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -22,89 +24,88 @@ import javax.validation.constraints.Size;
  */
 @Entity
 @Table(name = "medio_pago")
-@NamedQueries({
-    @NamedQuery(name = "MedioPago.findAll", query = "SELECT m FROM MedioPago m"),
-    @NamedQuery(name = "MedioPago.findByMpId", query = "SELECT m FROM MedioPago m WHERE m.mpId = :mpId"),
-    @NamedQuery(name = "MedioPago.findByMpDescripcion", query = "SELECT m FROM MedioPago m WHERE m.mpDescripcion = :mpDescripcion"),
-    @NamedQuery(name = "MedioPago.findByMpReqReferencia", query = "SELECT m FROM MedioPago m WHERE m.mpReqReferencia = :mpReqReferencia")})
+@NamedQueries({ @NamedQuery(name = "MedioPago.findAll", query = "SELECT m FROM MedioPago m"),
+		@NamedQuery(name = "MedioPago.findByMpId", query = "SELECT m FROM MedioPago m WHERE m.mpId = :mpId"),
+		@NamedQuery(name = "MedioPago.findByMpDescripcion", query = "SELECT m FROM MedioPago m WHERE m.mpDescripcion = :mpDescripcion"),
+		@NamedQuery(name = "MedioPago.findByMpReqReferencia", query = "SELECT m FROM MedioPago m WHERE m.mpReqReferencia = :mpReqReferencia") })
 public class MedioPago implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "mp_id")
-    private Integer mpId;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 100)
-    @Column(name = "mp_descripcion")
-    private String mpDescripcion;
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "mp_req_referencia")
-    private boolean mpReqReferencia;
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "mp_id")
+	private Integer mpId;
+	@Basic(optional = false)
+	@NotNull
+	@Size(min = 1, max = 100)
+	@Column(name = "mp_descripcion")
+	private String mpDescripcion;
+	@Basic(optional = false)
+	@NotNull
+	@Column(name = "mp_req_referencia")
+	private boolean mpReqReferencia;
 
-    public MedioPago() {
-    }
+	public MedioPago() {
+	}
 
-    public MedioPago(Integer mpId) {
-        this.mpId = mpId;
-    }
+	public MedioPago(Integer mpId) {
+		this.mpId = mpId;
+	}
 
-    public MedioPago(Integer mpId, String mpDescripcion, boolean mpReqReferencia) {
-        this.mpId = mpId;
-        this.mpDescripcion = mpDescripcion;
-        this.mpReqReferencia = mpReqReferencia;
-    }
+	public MedioPago(Integer mpId, String mpDescripcion, boolean mpReqReferencia) {
+		this.mpId = mpId;
+		this.mpDescripcion = mpDescripcion;
+		this.mpReqReferencia = mpReqReferencia;
+	}
 
-    public Integer getMpId() {
-        return mpId;
-    }
+	public Integer getMpId() {
+		return mpId;
+	}
 
-    public void setMpId(Integer mpId) {
-        this.mpId = mpId;
-    }
+	public void setMpId(Integer mpId) {
+		this.mpId = mpId;
+	}
 
-    public String getMpDescripcion() {
-        return mpDescripcion;
-    }
+	public String getMpDescripcion() {
+		return mpDescripcion;
+	}
 
-    public void setMpDescripcion(String mpDescripcion) {
-        this.mpDescripcion = mpDescripcion;
-    }
+	public void setMpDescripcion(String mpDescripcion) {
+		this.mpDescripcion = mpDescripcion;
+	}
 
-    public boolean getMpReqReferencia() {
-        return mpReqReferencia;
-    }
+	public boolean getMpReqReferencia() {
+		return mpReqReferencia;
+	}
 
-    public void setMpReqReferencia(boolean mpReqReferencia) {
-        this.mpReqReferencia = mpReqReferencia;
-    }
+	public void setMpReqReferencia(boolean mpReqReferencia) {
+		this.mpReqReferencia = mpReqReferencia;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (mpId != null ? mpId.hashCode() : 0);
-        return hash;
-    }
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (mpId != null ? mpId.hashCode() : 0);
+		return hash;
+	}
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof MedioPago)) {
-            return false;
-        }
-        MedioPago other = (MedioPago) object;
-        if ((this.mpId == null && other.mpId != null) || (this.mpId != null && !this.mpId.equals(other.mpId))) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(Object object) {
+		// TODO: Warning - this method won't work in the case the id fields are not set
+		if (!(object instanceof MedioPago)) {
+			return false;
+		}
+		MedioPago other = (MedioPago) object;
+		if ((this.mpId == null && other.mpId != null) || (this.mpId != null && !this.mpId.equals(other.mpId))) {
+			return false;
+		}
+		return true;
+	}
 
-    @Override
-    public String toString() {
-        return "mx.com.ferbo.model.MedioPago[ mpId=" + mpId + " ]";
-    }
-    
+	@Override
+	public String toString() {
+		return "mx.com.ferbo.model.MedioPago[ mpId=" + mpId + " ]";
+	}
+
 }
