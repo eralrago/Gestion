7311ad28d5cacb0f13f9bdbaf0a1994d034498ea
Ernesto <eralrago@gmail.com>
2022-05-30 16:48:59 -0500

    Desarrollo de Localización

---
+++ src/main/java/mx/com/ferbo/dao/PaisesDAO.java
@@ -0,0 +1,54 @@
+package mx.com.ferbo.dao;
+
+import java.util.List;
+
+import mx.com.ferbo.commons.dao.IBaseDAO;
+import mx.com.ferbo.model.Cliente;
+import mx.com.ferbo.model.Paises;
+
+public class PaisesDAO extends IBaseDAO<Paises, Integer> {
+
+	@Override
+	public Paises buscarPorId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Paises> buscarTodos() {
+		List<Paises> listado = null;
+		listado = em.createNamedQuery("Paises.findAll", Paises.class).getResultList();
+		return listado;
+	}
+
+	@Override
+	public List<Paises> buscarPorCriterios(Paises e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String actualizar(Paises e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String guardar(Paises e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminar(Paises e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminarListado(List<Paises> listado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
