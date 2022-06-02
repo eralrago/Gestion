847719fd962c11e01108db642ccde2df869bd8bf
LuisFer-AL <luisavila22@aragon.unam.mx>
2022-06-01 19:54:36 -0500

    Cambios pantalla

--- src/main/java/mx/com/ferbo/controller/PosicionCamaraBean.java
+++ src/main/java/mx/com/ferbo/controller/PosicionCamaraBean.java
@@ -8,8 +8,16 @@ import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import mx.com.ferbo.dao.CamaraDAO;
+import mx.com.ferbo.dao.PlantaDAO;
 import mx.com.ferbo.dao.PosicionCamaraDAO;
+import mx.com.ferbo.model.Camara;
+import mx.com.ferbo.model.Planta;
 import mx.com.ferbo.model.Posicion;
 
 @Named
@@ -22,13 +30,123 @@ public class PosicionCamaraBean implements Serializable {
 	private List<Posicion> posiciones;
 	private Posicion selectPosicion;
 	
+	private List<Planta> plantas;
+	private PlantaDAO plantasDao;
+	private CamaraDAO camaraDao;
+	private List<Camara> camaras;
+	private Camara camaraSelect;
+	private Planta plantaSelect;
+	private List<Camara> camaraPorPlanta;
+	private List<Posicion> listaPosiciones;
 	
-
-
+	
 	public PosicionCamaraBean() {
+		plantasDao = new PlantaDAO();
+		camaraDao = new CamaraDAO();
 		result = new PosicionCamaraDAO();
 		this.posiciones = result.findAll();
+		plantas = plantasDao.findall();
+		camaras = camaraDao.buscarTodos();
+		camaraPorPlanta = new ArrayList<Camara>();
+		listaPosiciones = new ArrayList<Posicion>();
 	}
+	
+	public void filtraListado() {
+		camaraPorPlanta.clear();
+		camaraPorPlanta = camaras.stream()
+				.filter(ps -> plantaSelect != null
+						? (ps.getPlantaCve().getPlantaCve().intValue() == plantaSelect.getPlantaCve().intValue())
+						: false)
+				.collect(Collectors.toList());
+		System.out.println("Productos Cliente Filtrados:" + camaraPorPlanta.toString());
+	}
+	
+	public void filtrarPosiciones() {
+		listaPosiciones.clear();
+		listaPosiciones = posiciones.stream()
+				.filter(cs -> camaraSelect != null
+				? (cs.getIdCamara() == camaraSelect.getCamaraCve())
+				: false)
+		.collect(Collectors.toList()); 
+		
+		PrimeFaces.current().ajax().update("form:dt-posiciones");
+	}
+
+	
+	
+	
+	public List<Posicion> getListaPosiciones() {
+		return listaPosiciones;
+	}
+
+	public void setListaPosiciones(List<Posicion> listaPosiciones) {
+		this.listaPosiciones = listaPosiciones;
+	}
+
+	public List<Camara> getCamaraPorPlanta() {
+		return camaraPorPlanta;
+	}
+
+	public void setCamaraPorPlanta(List<Camara> camaraPorPlanta) {
+		this.camaraPorPlanta = camaraPorPlanta;
+	}
+		
+
+	public PlantaDAO getPlantasDao() {
+		return plantasDao;
+	}
+
+	public void setPlantasDao(PlantaDAO plantasDao) {
+		this.plantasDao = plantasDao;
+	}
+
+	public CamaraDAO getCamaraDao() {
+		return camaraDao;
+	}
+
+	public void setCamaraDao(CamaraDAO camaraDao) {
+		this.camaraDao = camaraDao;
+	}
+
+	public List<Planta> getPlantas() {
+		return plantas;
+	}
+
+
+	public void setPlantas(List<Planta> plantas) {
+		this.plantas = plantas;
+	}
+
+
+	public List<Camara> getCamaras() {
+		return camaras;
+	}
+
+
+	public void setCamaras(List<Camara> camaras) {
+		this.camaras = camaras;
+	}
+
+
+	public Camara getCamaraSelect() {
+		return camaraSelect;
+	}
+
+
+	public void setCamaraSelect(Camara camaraSelect) {
+		this.camaraSelect = camaraSelect;
+	}
+
+
+	public Planta getPlantaSelect() {
+		return plantaSelect;
+	}
+
+
+	public void setPlantaSelect(Planta plantaSelect) {
+		this.plantaSelect = plantaSelect;
+	}
+
 
 
 	public List<Posicion> getAll() {
