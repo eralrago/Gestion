3cafb561bd373e3d8fa700fb31be2d8d74fc9a8e
Juanzassu <banano_zassu@hotmail.com>
2022-06-22 16:43:59 -0500

    Se generan componentes para Kardex

---
+++ src/main/java/mx/com/ferbo/dao/ConstanciaDeDepositoDAO.java
@@ -0,0 +1,53 @@
+package mx.com.ferbo.dao;
+
+import java.util.List;
+
+import mx.com.ferbo.commons.dao.IBaseDAO;
+import mx.com.ferbo.model.ConstanciaDeDeposito;
+
+public class ConstanciaDeDepositoDAO extends IBaseDAO<ConstanciaDeDeposito, Integer>{
+
+	@Override
+	public ConstanciaDeDeposito buscarPorId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ConstanciaDeDeposito> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ConstanciaDeDeposito> buscarPorCriterios(ConstanciaDeDeposito e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String actualizar(ConstanciaDeDeposito e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String guardar(ConstanciaDeDeposito e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminar(ConstanciaDeDeposito e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminarListado(List<ConstanciaDeDeposito> listado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
---
+++ src/main/java/mx/com/ferbo/dao/PartidaDAO.java
@@ -0,0 +1,52 @@
+package mx.com.ferbo.dao;
+
+import java.util.List;
+
+import mx.com.ferbo.commons.dao.IBaseDAO;
+import mx.com.ferbo.model.Partida;
+
+public class PartidaDAO extends IBaseDAO<Partida, Integer>{
+
+	@Override
+	public Partida buscarPorId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Partida> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Partida> buscarPorCriterios(Partida e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String actualizar(Partida e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String guardar(Partida e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminar(Partida e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminarListado(List<Partida> listado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
