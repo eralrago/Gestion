9765eaa9ea6b8ca3c8b49fe62b2cb69bbd003fef
Juanzassu <banano_zassu@hotmail.com>
2022-07-08 21:56:13 -0500

    Se mejora estructura se genera traspaso

--- src/main/java/mx/com/ferbo/util/KardexTotalsBean.java
+++ src/main/java/mx/com/ferbo/util/KardexTotalsBean.java
@@ -2,12 +2,15 @@ package mx.com.ferbo.util;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import mx.com.ferbo.model.ConstanciaDeDeposito;
 import mx.com.ferbo.model.DetalleConstanciaSalida;
+import mx.com.ferbo.model.Partida;
 
 @Named
 @ViewScoped
@@ -22,17 +25,28 @@ public class KardexTotalsBean implements Serializable {
 	 * Objetos para Detalle de Constancia de Salida
 	 */
 	private DetalleConstanciaSalida detalleSalidaSelected;
+	private List<DetalleConstanciaSalida> listDetalleSalida;
+	
+	/**
+	 * Objetos para salidas
+	 */
+	private List<Partida> listPartida;
+	private Partida partSelected;
 	
 	/**
 	 * Objetos para Constancia Deposito
 	 */
 	private ConstanciaDeDeposito consDepKardex;
+	private List<ConstanciaDeDeposito> listConsDepKardex;
 	
 	/**
 	 * Objetos para totales
 	 */
 	private Integer cantidadSalida;
 	private BigDecimal pesoSalida;
+	private Integer cantidadTotal;
+	private BigDecimal pesoTotal;
+	private String unidad;
 	
 	/**
 	 * constructor
@@ -40,6 +54,9 @@ public class KardexTotalsBean implements Serializable {
 	public KardexTotalsBean() {
 		detalleSalidaSelected = new DetalleConstanciaSalida();
 		consDepKardex = new ConstanciaDeDeposito();
+		listDetalleSalida = new ArrayList<>();
+		listPartida = new ArrayList<>();
+		listConsDepKardex = new ArrayList<>();
 	}
 	
 	public DetalleConstanciaSalida getDetalleSalidaSelected() {
@@ -67,6 +84,63 @@ public class KardexTotalsBean implements Serializable {
 
 	public void setConsDepKardex(ConstanciaDeDeposito consDepKardex) {
 		this.consDepKardex = consDepKardex;
+	}
+
+	public List<DetalleConstanciaSalida> getListDetalleSalida() {
+		return listDetalleSalida;
+	}
+
+	public void setListDetalleSalida(List<DetalleConstanciaSalida> listDetalleSalida) {
+		this.listDetalleSalida = listDetalleSalida;
+	}
+
+	public List<Partida> getListPartida() {
+		return listPartida;
+	}
+
+	public void setListPartida(List<Partida> listPartida) {
+		this.listPartida = listPartida;
+	}
+
+	public List<ConstanciaDeDeposito> getListConsDepKardex() {
+		return listConsDepKardex;
+	}
+
+	public void setListConsDepKardex(List<ConstanciaDeDeposito> listConsDepKardex) {
+		this.listConsDepKardex = listConsDepKardex;
+	}
+
+	public Partida getPartSelected() {
+		return partSelected;
+	}
+
+	public void setPartSelected(Partida partSelected) {
+		this.partSelected = partSelected;
+	}
+
+	public Integer getCantidadTotal() {
+		return cantidadTotal;
+	}
+
+	public void setCantidadTotal(Integer cantidadTotal) {
+		this.cantidadTotal = cantidadTotal;
+	}
+
+	public BigDecimal getPesoTotal() {
+		return pesoTotal;
+	}
+
+	public void setPesoTotal(BigDecimal pesoTotal) {
+		this.pesoTotal = pesoTotal;
+	}
+
+	public String getUnidad() {
+		return unidad;
+	}
+
+	public void setUnidad(String unidad) {
+		this.unidad = unidad;
 	}		
+	
 
 }
